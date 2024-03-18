e3cfd730c8845f7dbadd304cf5f01c299db8934c
Taashev <taashev.a.r92@yandex.ru>
2024-03-18 20:45:07 +0300

    DEV-101: Настройка CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 
+import { corsConfig } from './configs/cors-config';
+
 import { AppModule } from './app.module';
 
 import { TypeOrmExceptionFilter } from './exception-filters/typeorm.exception.filter';
@@ -11,6 +13,8 @@ import { GlobalExceptionFilter } from './exception-filters/global.exception.filt
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(corsConfig);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
